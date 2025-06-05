f46554d4c739e596feb6f29999fb6574824bf662
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-06-05 13:33:17 +0200

    Added Interface

---
+++ IdentityDemo.Application/Users/IIdentityUserService.cs
@@ -0,0 +1,12 @@
+﻿using IdentityDemo.Application.Dtos;
+using IdentityDemo.Application.Users;
+//using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDemo.Application.Users
+{
+    public interface IIdentityUserService
+    {
+        public Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password);
+       
+    }
+}
---
+++ IdentityDemo.Infrastructure/Services/IdentityUserService.cs
@@ -0,0 +1,32 @@
+﻿using IdentityDemo.Application.Dtos;
+using IdentityDemo.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Infrastructure.Services
+{
+    public class IdentityUserService // Infrastructure-servicen
+(
+UserManager<ApplicationUser> userManager, // Hanterar användare
+SignInManager<ApplicationUser> signInManager, // Hanterar inlogging
+RoleManager<IdentityRole> roleManager // Hanterar roller
+) : IIdentityUserService
+    {
+        public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
+        {
+            var result = await userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = user.Email,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            }, password);
+            return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
+        }
+        //..//
+    }
+}
